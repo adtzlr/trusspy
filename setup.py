55854e21f355151b1f032673350a5ed0938082f4
Andreas D <a.dutzler@gmail.com>
2020-04-09 15:04:51 +0200

    raise setup to version -post7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as f:
 
 setup(
     name='trusspy',
-    version='20180816-6',
+    version='20180816-7',
     description='Truss Solver for Python',
     long_description=readme,
     long_description_content_type='text/x-rst',
