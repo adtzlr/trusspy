72f3f0cd8c5000cef2b6fe7cd3e29a3664f5d60c
Andreas Dutzler <a.dutzler@gmail.com>
2023-03-15 22:49:28 +0100

    Update tpsolver.py

--- src/trusspy/solvers/tpsolver.py
+++ src/trusspy/solvers/tpsolver.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul 30 17:36:41 2018
-
-@author: adutz
+title: TrussPy - Truss Solver for Python
+author: Andreas Dutzler
+year: 2023
 """
 
 import copy
