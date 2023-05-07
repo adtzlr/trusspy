1ab8b773dc82043c7a113eabbcf8088f1c253787
Andreas Dutzler <a.dutzler@gmail.com>
2023-05-08 01:01:03 +0200

    Update model.py

--- src/trusspy/model.py
+++ src/trusspy/model.py
@@ -407,7 +407,7 @@ Dutzler Andreas, Graz University of Technology, 2023
         self.Results.duplicate_first_increment()
         self.Results.R[0].U = self.Results.R[0].U0.copy()
         self.Results.R[0].r[:] = 0
-        self.Results.R[0].ExtForces.forces [:] = 0
+        self.Results.R[0].ExtForces.forces[:] = 0
         self.Results.R[0].lpf = 0
 
         time_dclock_run = time.perf_counter() - self.clock0_run
