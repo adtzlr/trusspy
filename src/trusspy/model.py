9551ff1a58a63c6690c6a96e07ac1c15cf91bc1a
Andreas Dutzler <a.dutzler@gmail.com>
2023-05-07 17:33:21 +0200

    Update model.py

--- src/trusspy/model.py
+++ src/trusspy/model.py
@@ -417,6 +417,8 @@ Dutzler Andreas, Graz University of Technology, 2023
 
         # duplicate first increment to get right indices
         self.Results.duplicate_first_increment()
+        self.Results.R[0].U = self.Results.R[0].U0.copy()
+        self.Results.R[0].lpf = 0
         
         time_dclock_run = time.perf_counter() - self.clock0_run
         time_dtime_run = time.process_time() - self.time0_run
